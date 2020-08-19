0eb9113f40795ab0935b95e7d7f14ac8f6d5dea5
zenglenn42 <gsnospampls@pobox.com>
2020-08-19 13:51:12 -0700

    play with concept of layout reducer
    I'm trying to extend the notion of state reduction to layout reduction in the service
    of 'sub-accordions' that can expand to reveal nested items.  The use case would be
    a menu drawer of clickable items that may represent:
    
      * application routes that affect what is rendered in a content window
      * subdrawers of related items
    
    This commit represents a useful failure.
    
    I'm trying to related the behavior reducers and layout reducers as peers whereas the layout
    reducer really should be a downstream /observer/ of the openedItems array managed and mutated
    by the behavior reducer.
    
    As is, you get no expand/collapse behavior when clicking on a hand.  The behavior
    reducers do fire appropriately, they just has no linkage to the layout reducers and so
    the rendered react components remain unchanged.

--- src/components/hooks/Accordion.js
+++ src/components/hooks/Accordion.js
@@ -1,118 +1,20 @@
 import React from 'react'
-import posed from 'react-pose'
-import styled from '@emotion/styled'
-import {useAccordion, accordionReducer, actionTypes} from './useAccordion'
+import { useAccordion } from './useAccordion'
 
-function combineReducers(...stateReducers) {
-  return (state, action) => {
-    for (const stateReducer of stateReducers) {
-      const result = stateReducer(state, action)
-      if (result) return result
-    }
-  }
+function Accordion({
+  items,
+  behaviorReducer = () => {},
+  layoutReducer = () => {},
+  ...props
+}) {
+  const { getComponents } = useAccordion({ items })
+  // const { renderAccordion } = useAccordion(items)
+  // const components = renderAccordion()
+  // console.log('Accordion: ', components)
+  const components = getComponents()
+  // console.log('Accordion components:', components)
+
+  return <div>{components}</div>
 }
 
-function preventCloseReducer(openedItems, action) {
-  if (action.type === actionTypes.toggle_index) {
-    const closing = openedItems.includes(action.index)
-    const isLast = openedItems.length < 2
-    if (closing && isLast) {
-      return openedItems
-    }
-  }
-}
-
-function singleReducer(openedItems, action) {
-  if (action.type === actionTypes.toggle_index) {
-    const closing = openedItems.includes(action.index)
-    if (!closing) {
-      return [action.index]
-    }
-  }
-}
-
-const AccordionButton = styled('button')(
-  {
-    textAlign: 'left',
-    minWidth: 80,
-    cursor: 'pointer',
-    flex: 1,
-    paddingTop: 10,
-    paddingBottom: 10,
-    fontSize: 20,
-    border: 'none',
-    backgroundColor: 'unset',
-    ':focus': {
-      outline: 'none',
-      backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    },
-  },
-  ({isOpen}) =>
-    isOpen
-      ? {
-          backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        }
-      : null,
-)
-
-const PoseAccordionContents = posed.div({
-  open: {maxHeight: 200},
-  closed: {maxHeight: 0},
-})
-
-function AccordionContents({isOpen, ...props}) {
-  return (
-    <PoseAccordionContents
-      pose={isOpen ? 'open' : 'closed'}
-      style={{overflowY: 'hidden', textAlign: 'justify'}}
-      {...props}
-    />
-  )
-}
-
-const AccordionItem = styled('div')(
-  {
-    display: 'grid',
-    gridTemplate: 'auto auto',
-    gridGap: 4,
-    gridAutoFlow: 'row',
-  },
-  (props) => ({
-    gridAutoFlow: props.direction === 'horizontal' ? 'column' : 'row',
-  }),
-)
-
-function Accordion({items, stateReducer = () => {}, ...props}) {
-  const {openedItems, toggleIndex} = useAccordion({
-    stateReducer: combineReducers(stateReducer, accordionReducer),
-  })
-
-  return (
-    <div>
-      {items.map((item, index) => (
-        <AccordionItem key={item.title} direction="vertical">
-          <AccordionButton
-            isOpen={openedItems.includes(index)}
-            onClick={() => toggleIndex(index)}
-          >
-            {item.title}{' '}
-            <span>{openedItems.includes(index) ? '👇' : '👈'}</span>
-          </AccordionButton>
-          <AccordionContents isOpen={openedItems.includes(index)}>
-            {item.contents}
-          </AccordionContents>
-        </AccordionItem>
-      ))}
-    </div>
-  )
-}
-
-export {
-  Accordion,
-  AccordionButton,
-  AccordionContents,
-  AccordionItem,
-  combineReducers,
-  singleReducer,
-  preventCloseReducer,
-}
+export { Accordion }
--- src/components/hooks/useAccordion.js
+++ src/components/hooks/useAccordion.js
@@ -1,36 +1,181 @@
 import React from 'react'
+import posed from 'react-pose'
+import styled from '@emotion/styled'
 
-const actionTypes = {toggle_index: 'toggle_index'}
+const AccordionButton = styled('button')(
+  {
+    textAlign: 'left',
+    minWidth: 80,
+    cursor: 'pointer',
+    flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 20,
+    border: 'none',
+    backgroundColor: 'unset',
+    ':focus': {
+      outline: 'none',
+      backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    },
+  },
+  ({ isOpen }) =>
+    isOpen
+      ? {
+          backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        }
+      : null,
+)
 
-function accordionReducer(openedItems, action) {
+const PoseAccordionContents = posed.div({
+  open: { maxHeight: 200 },
+  closed: { maxHeight: 0 },
+})
+
+function AccordionContents({ isOpen, ...props }) {
+  return (
+    <PoseAccordionContents
+      pose={isOpen ? 'open' : 'closed'}
+      style={{ overflowY: 'hidden', textAlign: 'justify' }}
+      {...props}
+    />
+  )
+}
+
+const AccordionItem = styled('div')(
+  {
+    display: 'grid',
+    gridTemplate: 'auto auto',
+    gridGap: 4,
+    gridAutoFlow: 'row',
+  },
+  (props) => ({
+    gridAutoFlow: props.direction === 'horizontal' ? 'column' : 'row',
+  }),
+)
+
+const behaviorActionTypes = { toggle_index: 'toggle_index' }
+
+function defaultBehaviorReducer(openedItems, action) {
   switch (action.type) {
-    case actionTypes.toggle_index: {
-      console.log('curr state:', openedItems)
+    case behaviorActionTypes.toggle_index: {
       const closing = openedItems.includes(action.index)
-      console.log('closing:   ', closing)
       const nextOpenIndices = closing
         ? openedItems.filter((i) => i !== action.index)
         : [...openedItems, action.index]
-      console.log('next state:', nextOpenIndices)
       return nextOpenIndices
     }
     default: {
-      throw new Error('Unhandled type in accordionReducer: ' + action.type)
+      throw new Error(
+        'Unhandled type in Accordion defaultBehaviorReducer: ' + action.type,
+      )
     }
   }
 }
 
-function useAccordion({reducer = accordionReducer} = {}) {
-  const memoizedReducer = React.useCallback(reducer, [])
+function combineReducers(...reducers) {
+  return (state, action) => {
+    for (const reducer of reducers) {
+      const result = reducer(state, action)
+      if (result) return result
+    }
+  }
+}
+
+function preventCloseReducer(openedItems, action) {
+  if (action.type === behaviorActionTypes.toggle_index) {
+    const closing = openedItems.includes(action.index)
+    const isLast = openedItems.length < 2
+    if (closing && isLast) {
+      return openedItems
+    }
+  }
+}
+
+function singleReducer(openedItems, action) {
+  if (action.type === behaviorActionTypes.toggle_index) {
+    const closing = openedItems.includes(action.index)
+    if (!closing) {
+      return [action.index]
+    }
+  }
+}
+
+const layoutActionTypes = { map_items: 'map_items' }
+
+function defaultLayoutReducer(components, action) {
+  switch (action.type) {
+    case layoutActionTypes.map_items:
+      // console.log('defaultLayoutReducer action:', action)
+      console.log('typeof action.toggle:', action.toggle)
+      return action.items.map((item, index) => (
+        <AccordionItem key={item.title} direction="vertical">
+          <AccordionButton
+            isOpen={action.openedItems.includes(index)}
+            onClick={() => action.toggle(index)}
+          >
+            {item.title}{' '}
+            <span>{action.openedItems.includes(index) ? '👇' : '👈'}</span>
+          </AccordionButton>
+          <AccordionContents isOpen={action.openedItems.includes(index)}>
+            {item.contents}
+          </AccordionContents>
+        </AccordionItem>
+      ))
+    default: {
+      throw new Error(
+        'Unhandled type in Accordion defaultLayoutReducer: ' + action.type,
+      )
+    }
+  }
+}
+
+function useAccordion({
+  behaviorReducer = defaultBehaviorReducer,
+  layoutReducer = defaultLayoutReducer,
+  items = [],
+} = {}) {
+  const inputItems = React.useRef(items)
+  const memoizedBehaviorReducer = React.useCallback(behaviorReducer, [])
   const initialOpenedItems = []
-  const [openedItems, dispatch] = React.useReducer(
-    memoizedReducer,
+  const [openedItems, mutateOpenedItems] = React.useReducer(
+    memoizedBehaviorReducer,
     initialOpenedItems,
   )
   const toggleIndex = (index) => {
-    return dispatch({type: actionTypes.toggle_index, index: index})
+    mutateOpenedItems({
+      type: behaviorActionTypes.toggle_index,
+      index: index,
+    })
   }
-  return {openedItems, toggleIndex}
+
+  const memoizedLayoutReducer = React.useCallback(layoutReducer, [])
+  const [components, mutateComponents] = React.useReducer(
+    memoizedLayoutReducer,
+    [],
+  )
+
+  const getComponents = () => {
+    if (components.length === 0) {
+      mutateComponents({
+        type: layoutActionTypes.map_items,
+        items: inputItems.current,
+        toggle: toggleIndex,
+        openedItems: openedItems,
+      })
+    }
+    return components
+  }
+
+  return { getComponents }
 }
 
-export {useAccordion, accordionReducer, actionTypes}
+export {
+  useAccordion,
+  defaultBehaviorReducer,
+  preventCloseReducer,
+  singleReducer,
+  combineReducers,
+  behaviorActionTypes,
+  defaultLayoutReducer,
+  layoutActionTypes,
+}
