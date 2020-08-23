c8840ee49b79401df957c05829fddc1e2cab2f47
zenglenn42 <gsnospampls@pobox.com>
2020-08-23 09:18:37 -0700

    pass optional props (like expansion reducer and intially expanded items) to useAccordion

--- src/components/hooks/Accordion.js
+++ src/components/hooks/Accordion.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useAccordion } from './useAccordion'
 
-function Accordion({ items, ...props }) {
-  const { components } = useAccordion({ items })
+function Accordion(props) {
+  const { items, ...optional } = props
+  const { components } = useAccordion({ items, ...optional })
   return <div>{components}</div>
 }
 
