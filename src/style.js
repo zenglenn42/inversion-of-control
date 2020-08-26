d383bda28415c117caf185f35e1d395290c4a287
zenglenn42 <gsnospampls@pobox.com>
2020-08-26 10:39:06 -0700

    simplify border to gray instead of jarring image border

--- src/style.js
+++ src/style.js
@@ -56,11 +56,9 @@ export const subcontentLeft = {
   width: '100%',
   overflow: 'auto scroll',
   backgroundColor: 'white',
-  padding: '.75em',
+  padding: '1em',
   borderRadius: '.15em',
-  border: '0.75em solid transparent',
-  background:
-    'linear-gradient(white, white) padding-box, url(/images/fixed-border.jpg) border-box  0 / cover'
+  border: '0.4em solid #424242'
 }
 
 export const subcontentRight = {
@@ -71,9 +69,7 @@ export const subcontentRight = {
   width: '100%',
   overflow: 'auto scroll',
   backgroundColor: 'white',
+  padding: '1em',
   borderRadius: '.15em',
-  padding: '0.75em',
-  border: '0.75em solid transparent',
-  background:
-    'linear-gradient(white, white) padding-box, url(/images/recursive-border.jpg) border-box  0 / cover'
+  border: '0.4em solid #424242'
 }
